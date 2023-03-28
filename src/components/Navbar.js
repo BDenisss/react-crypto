d8c038ab3bd2b33c7cd2120a385e80e980ea4978
Denis Bucspun <denis.bucspun@edu.devinci.fr>
2023-03-28 16:02:57 +0200

    :sparkles: introduce the beginning of the project

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from  'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1>My App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
